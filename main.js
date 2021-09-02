3da2dc6429e1b4862c8a4ebaa5b9c26140c9b8a2
byronleetinker <byronleetinker03@gmail.com>
2021-09-02 16:25:07 +0200

    updating

--- main.js
+++ main.js
@@ -2,3 +2,42 @@
 function toggleNavbar() {
     document.getElementsByClassName("navbar-links")[0].classList.toggle("active");
   }
+
+  let loginButton = document.querySelector(".but1");
+let form = document.querySelector(".form");
+console.log(form);
+
+function login(username, password) {
+  console.log(username);
+  console.log(password);
+  fetch(" https://bookhub-bookstore.herokuapp.com/auth", {
+    method: "POST",
+    body: JSON.stringify({
+      username: `${username}`,
+      password: `${password}`,
+    }),
+    headers: {
+      "Content-Type": "application/json",
+    },
+  })
+    .then((response) => response.json())
+    .then((data) => {
+      if (data["access_token"]) {
+        console.log(data);
+        myStorage = window.localStorage;
+        myStorage.setItem("jwt-token", data["access_token"]);
+        myStorage.setItem("username", username);
+        myStorage.setItem("password", password);
+        window.location.href = "./shop.html";
+      }
+    });
+}
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let username = document.querySelector(".username").value;
+  let password = document.querySelector(".password").value;
+
+  login(username, password);
+});
